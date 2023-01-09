63d9fffa43426d96cdf1be46de6befdef4f2114c
Nahue <nahuel213@yahoo.com.ar>
2023-01-09 14:30:28 -0300

    Obtener sesion

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 
 @Controller
@@ -33,5 +36,35 @@ public class UsuarioController {
         Usuario nuevoUsuario = usuarioService.save(usuario);
         logger.info("usuario insertado {}",nuevoUsuario);
         return "redirect:/"; //Para que redireccione a la Home
+    }
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "usuario/login";
+    }
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session) {
+        //logger.info("Accesos: {}", usuario);
+
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+
+
+        //logger.info("Usuario obtenido: {}", user.get());
+
+        if( user.isPresent()) {
+            session.setAttribute("idUsuario",user.get().getId());
+            if (user.get().getTipo().equals("ADMIN")){
+                return "redirect:/administrador";
+            }
+            else{
+                return "redirect:/";
+            }
+
+        } else{
+            logger.info("Usuario no existe");
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/curso/ecommerce/repository/IUsuarioRepository.java
+++ src/main/java/com/curso/ecommerce/repository/IUsuarioRepository.java
@@ -3,5 +3,10 @@ package com.curso.ecommerce.repository;
 import com.curso.ecommerce.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+    //OJO EL IntelliJ te ayuda así:
+    //Optional<Usuario> findUsuarioByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
